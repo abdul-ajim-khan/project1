0cdf940727b5704c762858d4dc9966df3708bd86
abdul-ajim-khan <aak8389966@gmail.com>
2024-09-10 13:04:50 +0530

    5th commit

---
+++ index.py
@@ -0,0 +1,8 @@
+## 1) ARMSTRONG NUMBER
+num = input("Enter a number: ")
+count = len(num)
+total=0
+for b in range(count):
+    total=total + ((int(num[b])**3))
+if(total==int(num)):
+    print(num,"is an armstrong number.")
